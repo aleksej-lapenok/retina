c2fe7efab33c0a5da463fa1aa2e6d185e4f92587
Aleksej Lapenok <live-penguin@yandex-team.ru>
2020-07-30 16:44:32 +0300

    fix smooth loss

--- keras_retinanet/losses.py
+++ keras_retinanet/losses.py
@@ -16,6 +16,7 @@ limitations under the License.
 
 import keras
 import tensorflow as tf
+import numpy as np
 
 # slim = tf.contrib.slim
 from . import backend
@@ -145,8 +146,8 @@ def smooth_l1(sigma=3.0, sigma_var=None):
         regression_loss = backend.where(
             keras.backend.less(regression_diff, 1.0 / sigma_squared),
             factor * 0.5 * sigma_squared * keras.backend.pow(regression_diff, 2) + 0.5 * sigma_var,
-            keras.backend.sqrt(2) * (regression_diff - 0.5 / sigma_squared) / sigma_var -
-            keras.backend.log(1 / (keras.backend.sqrt(2) * sigma_var))
+            np.sqrt(2) * (regression_diff - 0.5 / sigma_squared) / sigma_var -
+            keras.backend.log(1 / (np.sqrt(2) * sigma_var))
         )
 
         # compute the normalizer: the number of positive anchors
