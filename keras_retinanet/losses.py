c1de57256128b88f21528e784b0c588f614d7019
Aleksej Lapenok <live-penguin@yandex-team.ru>
2020-08-09 12:24:38 +0300

    s->s^2 in smooth_l1

--- keras_retinanet/losses.py
+++ keras_retinanet/losses.py
@@ -142,11 +142,11 @@ def smooth_l1(sigma=3.0, sigma_var=None):
         regression_diff = regression - regression_target
         regression_diff = keras.backend.abs(regression_diff)
 
-        factor = 1.0 / (2.0 * keras.backend.exp(sigma_var))
+        factor = 1.0 / (2.0 * keras.backend.exp(sigma_var * sigma_var))
         regression_loss = backend.where(
             keras.backend.less(regression_diff, 1.0 / sigma_squared),
-            factor * sigma_squared * keras.backend.pow(regression_diff, 2) + 0.5 * sigma_var,
-            np.sqrt(2) * (regression_diff - 0.5 / sigma_squared) / keras.backend.sqrt(keras.backend.exp(sigma_var)) + 0.5 * sigma_var
+            factor * sigma_squared * keras.backend.pow(regression_diff, 2) + 0.5 * keras.backend.pow(sigma_var, 2),
+            np.sqrt(2) * (regression_diff - 0.5 / sigma_squared) / keras.backend.sqrt(keras.backend.exp(sigma_var * sigma_var)) + 0.5 * sigma_var * sigma_var
         )
 
         # compute the normalizer: the number of positive anchors
