d68706a2f9b49516057073622a053f279406bf73
Aleksej Lapenok <live-penguin@yandex-team.ru>
2020-10-19 16:49:48 +0300

    fix mult_factor & cycle_length

--- keras_retinanet/bin/train.py
+++ keras_retinanet/bin/train.py
@@ -225,8 +225,8 @@ def create_callbacks(model, training_model, prediction_model, validation_generat
     # callbacks.append(LRFinder(max_lr=args.lr, min_lr=args.lr / 10000, steps_per_epoch=args.steps, epochs=args.epochs))
     callbacks.append(SGDRScheduler(min_lr=1e-30, max_lr=args.lr, steps_per_epoch=args.steps,
                                    lr_decay=PolynomialDecay(args.epochs),
-                                   cycle_length=args.epochs / 10,
-                                   mult_factor=1.5))
+                                   cycle_length=5,
+                                   mult_factor=1))
     # callbacks.append(keras.callbacks.LearningRateScheduler(PolynomialDecay(args.epochs, initAlpha=args.lr), verbose=1))
     return callbacks
 
