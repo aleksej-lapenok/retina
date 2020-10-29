12a281f72ccc5f76e7d2f49faae3f24850241568
Aleksej Lapenok <live-penguin@yandex-team.ru>
2020-10-29 13:30:13 +0300

    fix log

--- keras_retinanet/callbacks/SGDRScheduler.py
+++ keras_retinanet/callbacks/SGDRScheduler.py
@@ -81,7 +81,7 @@ class SGDRScheduler(Callback):
             self.next_restart += self.cycle_length
             self.max_lr *= decay
 
-            print('SGDRScheduler :: set max lr in ' + self.max_lr)
+            print('SGDRScheduler :: set max lr in ', self.max_lr)
 
     # def on_train_end(self, logs={}):
     #     '''Set weights to the values from the end of the most recent cycle for best performance.'''
